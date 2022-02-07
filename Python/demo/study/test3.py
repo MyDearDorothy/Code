420aaa784e0d384ba107d04df9b2d64e3d6b9bf4
MyDearDorothy <2929167926@qq.com>
2022-02-07 18:33:35 +0800

    New

--- Python/demo/study/test3.py
+++ Python/demo/study/test3.py
@@ -6,7 +6,7 @@ import os
 class fileHandler:
 
     def __init__(self):
-        self.path=".\\file\Readme.txt"
+        self.path= "../file/Readme.txt"
 
     # 注意，使用open()函数打开的文件对象，必须手动进行关闭（后续章节会详细讲解），Python垃圾回收机制无法自动回收打开文件所占用的资源。
     def openFile(self):
